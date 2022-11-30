b14251340834261d6dc207ca16ff535a394eb582
Kolibrin1 <112856454+Kolibrin1@users.noreply.github.com>
2022-11-30 18:36:59 +0300

    Add files via upload

---
+++ udpClient.cpp
@@ -0,0 +1,99 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <string>
+#include <windows.h>
+#include <iostream>
+#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable: 4996)
+using namespace std;
+#define PORT 1234
+
+// CLIENT
+
+struct goods
+{
+    string nazv;
+    int price;
+}B;
+
+int main(int argc, char* argv[])
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "Введите ip адрес сервера" << endl;
+    string ip = "192.168.1.85";
+    cin >> ip;
+    const char* SERVERADDR = ip.c_str();
+    char buff[10 * 1014];
+    cout << "UDP Demo Client";
+
+    // Winsocks lib _init_
+    if (WSAStartup(0x202, (WSADATA*)&buff))
+    {
+        cout << "WSASTARTUP ERROR: " << WSAGetLastError() << "\n";
+        return -1;
+    }
+
+    // открытие сокета
+    SOCKET my_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (my_sock == INVALID_SOCKET) {
+        cout << "SOCKET() ERROR: " << WSAGetLastError() << "\n";
+        WSACleanup();
+        return -1;
+    }
+
+    // СОЕДИНЕНИЕ
+    HOSTENT* hst;
+    sockaddr_in Daddr;
+    Daddr.sin_family = AF_INET;
+    Daddr.sin_port = htons(PORT);
+
+    // определение IP-адреса узла
+    if (inet_addr(SERVERADDR))
+        Daddr.sin_addr.s_addr = inet_addr(SERVERADDR);
+    else
+        if (hst = gethostbyname(SERVERADDR))
+            Daddr.sin_addr.s_addr = ((unsigned long**)
+                hst->h_addr_list)[0][0];
+        else {
+            cout << "Unknown Host: " << WSAGetLastError() << "\n";
+            closesocket(my_sock);
+            WSACleanup();
+            return -1;
+        }
+
+    // ОБМЕН ДАННЫХ
+    while (1) {
+        string Stroka;
+        cout << "Продолжить? (y/n)  " << endl;
+        cin >> Stroka;
+        if (Stroka == "n") break;
+
+        // ВВОД ДАННЫХ
+        cout << "Введите название товара и его цену" << endl;
+        cin >> B.nazv;
+        cin >> B.price;
+        getchar();
+
+        // ОТПРАВКА СООБЩЕНИЙ СЕРВЕРУ
+        sendto(my_sock, (char*)&B, sizeof(B), 0,
+            (sockaddr*)&Daddr, sizeof(Daddr));
+
+        // ПРИЕМ ОТВЕТА ОТ СЕРВЕРА
+        sockaddr_in SRaddr;
+        int SRaddr_size = sizeof(SRaddr);
+        int n = recvfrom(my_sock, buff, sizeof(buff), 0,
+            (sockaddr*)&SRaddr, &SRaddr_size);
+        if (n == SOCKET_ERROR) {
+            cout << "RECVFROM() ERROR:" << WSAGetLastError() << "\n";
+            closesocket(my_sock);
+            WSACleanup();  return -1;
+        }
+        buff[n] = '\0';
+        cout << buff << "\n";
+    }
+
+    closesocket(my_sock);
+    WSACleanup();
+    return 0;
+}
---
+++ udpServer.cpp
@@ -0,0 +1,85 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <windows.h>
+#include <string>
+#include <iostream>
+#pragma comment(lib, "Ws2_32.lib")
+#pragma warning(disable: 4996)
+using namespace std;
+#define PORT 1234    // порт сервера
+string answ = "empty";
+
+//SERVER
+
+struct goods
+{
+    string nazv;
+    int price;
+}A;
+
+int main(int argc, char* argv[]) {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    char buff[1024];
+    cout << "UDP DEMO ECHO-SERVER\n";
+    // Winsocks lib _init_
+    if (WSAStartup(0x202, (WSADATA*)&buff[0]))
+    {
+        cout << "WSAStartup error: " << WSAGetLastError();
+        return -1;
+    }
+    // открытие сокета
+    SOCKET Lsock;
+    Lsock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (Lsock == INVALID_SOCKET) {
+        cout << "SOCKET() ERROR: " << WSAGetLastError();
+        WSACleanup();
+        return -1;
+    }
+    // связывание сокета с локальным адресом 
+    sockaddr_in Laddr;
+    Laddr.sin_family = AF_INET;
+    Laddr.sin_addr.s_addr = INADDR_ANY;
+    Laddr.sin_port = htons(PORT);
+    if (bind(Lsock, (sockaddr*)&Laddr, sizeof(Laddr)))
+    {
+        cout << "BIND ERROR:" << WSAGetLastError();
+        closesocket(Lsock);
+        WSACleanup();
+        return -1;
+    }
+    // обработка пакетов, присланных клиентами
+    while (1) {
+        // Получение данных
+        sockaddr_in Caddr;
+        int Caddr_size = sizeof(Caddr);
+        int bsize = recvfrom(Lsock, (char*)&A, sizeof(buff) - 1, 0,
+            (sockaddr*)&Caddr, &Caddr_size);
+        cout << A.nazv << " " << A.price << endl;
+
+        if (bsize == SOCKET_ERROR)
+            cout << "RECVFROM() ERROR:" << WSAGetLastError();
+        // Определяем IP-адрес клиента и прочие атрибуты
+        HOSTENT* hst;
+        hst = gethostbyaddr((char*)&Caddr.sin_addr, 4, AF_INET);
+        cout << "NEW DATAGRAM!\n" <<
+            ((hst) ? hst->h_name : "Unknown host") << '\n' <<
+            inet_ntoa(Caddr.sin_addr) << '\n' << ntohs(Caddr.sin_port) << '\n';
+        buff[bsize] = '\0';              // добавление завершающего нуля
+
+        // посылка датаграммы клиенту
+        int discount = 0;
+        if (A.price >= 3000)
+            discount = 15;
+        else if (A.price >= 2000 && A.price < 3000)
+            discount = 10;
+        else if (A.price >= 1000 && A.price < 2000)
+            discount = 5;
+        else
+            discount = 0;
+
+        answ = "Стоимость " + A.nazv + " с учетом скидки: " + to_string(A.price - discount * A.price / 100) + " рублей";
+
+        sendto(Lsock, (char*)&answ[0], answ.size(), 0, (sockaddr*)&Caddr, sizeof(Caddr));
+    }      return 0;
+}
